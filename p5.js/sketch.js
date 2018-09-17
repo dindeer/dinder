012abc4273a10420a48d66042336d75b6609e132
Albin <albin.lamichane9@gmail.com>
2018-09-17 08:29:33 +0545

    final changes

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -2,12 +2,22 @@ var gather;
 function preload()
 {
    gather= loadJSON("webscrapping/student_content2020.json");
+   nome=   loadJSON("webscrapping/name_2020.json");
+   console.log(nome);
 
 
 }
   function setup()
   {
     noCanvas();
-    var data=gather.content[1].HighSchool[1];
-    createP(data);
+
+    var data=gather.content[1].HighSchool;
+    var name=nome.infor[1].name;
+    //createP(name);
+   //createP(data);
+   var loop = nome.infor;
+   for(var i =0;i<loop.length;i++)
+   {
+     createElement('h1',loop[i].name);
+   }
   }
