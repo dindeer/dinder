ab4ea74dbde900b849ae95efd2f8e37093718ed8
Albin <albin.lamichane9@gmail.com>
2018-09-17 08:43:46 +0545

    changes

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -3,7 +3,7 @@ function preload()
 {
    gather= loadJSON("webscrapping/student_content2020.json");
    nome=   loadJSON("webscrapping/name_2020.json");
-   console.log(nome);
+   //console.log(nome);
 
 
 }
@@ -16,8 +16,12 @@ function preload()
     //createP(name);
    //createP(data);
    var loop = nome.infor;
+   var con = gather.content;
+
    for(var i =0;i<loop.length;i++)
    {
-     createElement('h1',loop[i].name);
+     createElement('h2',loop[i].name);
+     //createElement('h2',con[j+1].HighSchool)
+
    }
   }
